840570f590f7ee10a8e24675f42ca9e0aa28abdd
camerajunkie <ebenofthecamera@gmail.com>
2022-02-27 22:27:13 -0500

    Implement get command, modify go.mod, and create TaskFile.yml
    1) implement logic in get command to return pickleName
    2) Fix go.mod to reduce the amount of indirect references required for go-pickle-cli
    3) Implement TaskFile.yml for run, build, and clean duties

--- cmd/get.go
+++ cmd/get.go
@@ -6,6 +6,9 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -13,28 +16,49 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This command will get the desired Pickle Rick",
+	Long:  `This get command will call GitHub repo in order to return the desired Pickle Rick`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		var pickleName = "arakaki-pickle.png"
+
+		if len(args) >= 1 && args[0] != "" {
+			pickleName = args[0]
+		}
+
+		URL := "https://raw.githubusercontent.com/cameracode/ricksofpickle/Develop/" + pickleName + ".png"
+
+		fmt.Println("Try to get: `" + pickleName + "` Pickle Rick...")
+
+		// Get the data
+		response, err := http.Get(URL)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// defer closes connection
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			// Create the file
+			out, err := os.Create(pickleName + ".png")
+			if err != nil {
+				fmt.Println(err)
+			}
+			// defer closes connection
+			defer out.Close()
+
+			// Write response body to file
+			_, err = io.Copy(out, response.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Perfect! Just saved in " + out.Name() + "!")
+		} else {
+			fmt.Println("Error:  " + pickleName + " not exists! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
